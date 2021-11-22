22a94be8eb8a5d7e88d2457dd77d3689030b0363
Martinet Laure <laure.martinet@laplateforme.io>
2021-11-22 17:13:33 +0100

    img

--- projet1/index.php
+++ projet1/index.php
@@ -5,10 +5,21 @@
       <meta charset="utf-8">
       <title>Accueil</title>
    </head>
+   <header>
    <h1>Accueil</h1>
+   </header>
+
    <div class="link">
       <a href="inscription.php"><input type="button" value="Inscription"></a>
       <p>OU</p>
       <a href="connexion.php"><input type="button" value="Se connecter"></a>
    </div>
+
+   <footer>
+      <div id="github">
+         <a href="https://github.com/laure-martinet/module-connexion">
+            <img src="imggit.png" width="300" height="200">
+         </a>
+      </div>
+   </footer>
       </html>
